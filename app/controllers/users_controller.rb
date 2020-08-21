a2b9fb36db040c1ff794d02e846e5f9787579c36
keiba804 <mottuso0017@gmail.com>
2020-08-21 11:04:04 +0000

    add views books/top,about

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = user.all
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def destroy
+	end
+
+	def update
+	end
+end
