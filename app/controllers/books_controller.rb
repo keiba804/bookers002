195e4b7b7120e82d01a23cd819200fe665d54fb8
keiba804 <mottuso0017@gmail.com>
2020-08-26 14:25:26 +0000

    edit books/views,controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,13 @@ class BooksController < ApplicationController
 	end
 
 	def show
+		@user = User.find(params[:id])
+		@book = Book.find(params[:id])
+		@newbook = Book.new
 	end
 
 	def edit
+		@book = Book.find(params[:id])
 	end
 
 	def create
@@ -24,6 +28,17 @@ class BooksController < ApplicationController
 	end
 
 	def update
+		@book = Book.find(params[:id])
+	    if @book.update(book_params)
+	    	redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+	    else
+	    	render 'edit'
+	    end
 	end
+
+	private
+		def book_params
+			params.require(:book).permit(:title, :body)
+		end
 
 end
